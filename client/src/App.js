f18e93ef7fbe49bb8298c597497d1b8daf4d3cfd
William Schwindt <williammschwindt@gmail.com>
2020-03-22 16:35:26 -0700

    login form created

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import BubblePage from './components/BubblePage';
 
 import Login from "./components/Login";
 import "./styles.scss";
@@ -9,10 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <Route exact path="/bubblepage" component={BubblePage} />
       </div>
     </Router>
   );
